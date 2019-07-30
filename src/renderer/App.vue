5834e9afef889956d95f89402897ace96f32021f
linwang5 <linwang5@iflytek.com>
2019-07-30 21:27:10 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'cvs2pen2cvs'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
